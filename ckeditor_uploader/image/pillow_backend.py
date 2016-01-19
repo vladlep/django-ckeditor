dc7fb74e323fb9ef00c76e4de907585b8bafde5a
¨Vlad <vlad.lep@gmail.com>
2016-01-19 12:16:59 +0100

    update default storage to custom storage for thumbnail creation

--- ckeditor_uploader/image/pillow_backend.py
+++ ckeditor_uploader/image/pillow_backend.py
@@ -4,6 +4,7 @@ import os
 from io import BytesIO
 
 from django.conf import settings
+
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from PIL import Image, ImageOps
@@ -26,6 +27,7 @@ def create_thumbnail(file_path):
     thumbnail_format = utils.get_image_format(os.path.splitext(file_path)[1])
 
     image = storage.open(file_path)
+
     image = Image.open(image)
     file_format = image.format
 
@@ -49,11 +51,19 @@ def create_thumbnail(file_path):
         None)
     thumbnail.seek(0)
 
+<<<<<<< HEAD:ckeditor_uploader/image/pillow_backend.py
     return storage.save(thumbnail_filename, thumbnail)
 
 
 def should_create_thumbnail(file_path):
     image = storage.open(file_path)
+=======
+    return utils.get_storage().save(thumbnail_filename, thumbnail)
+
+
+def should_create_thumbnail(file_path):
+    image = utils.get_storage().open(file_path)
+>>>>>>> update default storage to custom storage for thumbnail creation:ckeditor/image/pillow_backend.py
     try:
         Image.open(image)
     except IOError:
